72fddc4b1e5d0b1fab2c3f4023565000b3e6397a
Alex Jarvis <alxjrvs@gmail.com>
2016-08-18 19:24:31 -0400

    Now with multiples

--- rollr.ex
+++ rollr.ex
@@ -1,8 +1,24 @@
 defmodule Rollr do
-  def roll(sides) do
-    IO.puts :rand.uniform(sides)
+  def roll(sides, num) do
+    roll_multiple(sides, num, 0)
+  end
+
+  defp roll_multiple(_sides, iterator, total) when iterator == 0 do
+    total 
+  end
+
+  defp roll_multiple(sides, iterator, total) do 
+    new_roll = roll_single(sides)
+    new_total = (new_roll + total)
+    new_iterator = (iterator - 1) 
+    roll_multiple(sides, new_iterator, new_total)
+  end
+
+
+  defp roll_single(sides) do
+    :rand.uniform(sides)
   end
 end
 
 
-IO.puts Rollr.roll(20)
+IO.puts Rollr.roll(20, 1)
